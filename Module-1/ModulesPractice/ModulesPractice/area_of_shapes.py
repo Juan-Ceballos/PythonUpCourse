bf8558a9aff99bbc063c0bfadd4a3e743ebb5d11
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2024-09-18 00:58:04 -0400

    practice variables

---
+++ Module-1/ModulesPractice/ModulesPractice/area_of_shapes.py
@@ -0,0 +1,8 @@
+def calculate_area_rect(length, width):
+    area = length * width
+    print(area)
+
+def calculate_area_square(a):
+    area = a * a
+    print(area)
+
---
+++ Module-1/ModulesPractice/ModulesPractice/main.py
@@ -0,0 +1,7 @@
+import time, random, area_of_shapes as shapes
+
+
+
+print(random.randint(0, 10))
+shapes.calculate_area_square(5)
+print("hello world")

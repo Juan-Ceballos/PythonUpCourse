cd6a7b4945161dc21945bef578638be882e41660
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2024-09-17 23:18:31 -0400

    operators practice

---
+++ Module-1/OperatorsPractice/OperatorsPractice/main.py
@@ -0,0 +1,35 @@
+#Operators
+from tabnanny import check
+
+# + Plus
+
+exampleAdd = 2 + 2
+print(exampleAdd)
+
+exampleSubtract = 2 - 2
+print(exampleSubtract)
+
+exampleMod = 2 % 2
+print(exampleMod)
+
+a = 20
+a = 5
+print(a)
+
+a += 5
+print(a)
+
+a -= 10
+print(a)
+
+checkEqual = 5 == 5
+print(checkEqual)
+
+checkNotEqual = 5 != 5
+print(checkNotEqual)
+
+checkLessThan = 5 < 3
+print(checkLessThan)
+
+checkWithAnd = 10 > 5 > 4 # and is keyword, also have or keyword for operators
+print(checkWithAnd)

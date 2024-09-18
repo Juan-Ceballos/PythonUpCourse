b27beeb8b02e49d6c89b0c2aa489376de767bb43
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2024-09-17 21:15:17 -0400

    practice variables

---
+++ Module-1/PracticeVariables/pythonProject/main.py
@@ -0,0 +1,25 @@
+name = "jack"
+print(name)
+name2 = 'jack'
+print(name2)
+meters = 10.5
+print(meters)
+age = 2
+print(age)
+isTrue = True
+print(isTrue)
+isFalse = False
+print(isFalse)
+isEqual = 2 == 2
+print(isEqual)
+notEqual = 2 == 3
+print(notEqual)
+
+print("hello " + name)
+
+# python object-oriented capable language, allows you to define classes. Python is object based,
+# everything considered objects including variables. Use objects and classes.
+
+print(type(name))
+
+print(name + " " + str(age))
